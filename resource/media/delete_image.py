0f6edae43c9cab94e575e64c6201fbd794e6756f
Alexis <alexis.prunier@securitymadein.lu>
2022-01-14 16:19:00 +0100

    Add media/delete_image resource

--- resource/media/delete_image.py
+++ resource/media/delete_image.py
@@ -3,10 +3,12 @@ from flask_apispec import use_kwargs, doc
 from flask_jwt_extended import jwt_required
 from flask_restful import Resource
 from webargs import fields
+import os
 
 from decorator.catch_exception import catch_exception
 from decorator.log_request import log_request
 from decorator.verify_admin_access import verify_admin_access
+from config.config import IMAGE_FOLDER
 
 
 class DeleteImage(MethodResource, Resource):
@@ -29,6 +31,11 @@ class DeleteImage(MethodResource, Resource):
     @verify_admin_access
     @catch_exception
     def post(self, **kwargs):
+
+        try:
+            os.remove(os.path.join(IMAGE_FOLDER, str(kwargs["id"])))
+        except FileNotFoundError:
+            pass
 
         self.db.delete(self.db.tables["Image"], {"id": kwargs["id"]})
 
